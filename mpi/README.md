25f6afc0c79157fc904b96c43701e1425d630c5f
David Froger <david.froger@inria.fr>
2015-05-04 22:01:18 +0200

    Remove random numbers

--- mpi/README.md
+++ mpi/README.md
@@ -31,5 +31,5 @@ We expected the program be run with wall clock:
 I get a problem with 7 or more processes with version `MPICH 3.1.4`, see results
 in `carla/` and `otis/`. (carla and otis are my machine names).
 
-With my real code, on `otis`, I have the same problem with `Open MPI 1.8.4`,
-`MPIC 1.4.1` and `MPICH 3.1.4`.
+In a real code (Computational Fluid Dynamic software), on `otis`, I have the
+same problem with `Open MPI 1.8.4`, `MPIC 1.4.1` and `MPICH 3.1.4`.
--- mpi/bench_mpi.cxx
+++ mpi/bench_mpi.cxx
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <unistd.h>
-#include <stdlib.h>
 
 #include <mpi.h>
 
 using namespace std;
+
+/* For example, with size 4 and overhead_percent_max 5:
+ *    
+ *            rank=0  rank=1  rank=2  rank=3
+ *    iter=0  0.      1.66    3.33    5.
+ *    iter=1  1.66    3.33    5.      0.
+ *    iter=2  3.33    5.      0.      1.66
+ *    iter=3  5.      0.      1.66    3.33
+ *    iter=4  0.      1.66    3.33    5.
+ *    iter=5  1.66    3.33    5.      0.
+ *    iter=6  3.33    5.      0.      1.66
+ *    etc
+ *
+ */
+double get_overhead_percent(int rank, int size, int iter,
+                            double overhead_percent_max)
+{
+    int turning_rank = (rank + iter) % size;
+    return double(turning_rank) / (size-1) * overhead_percent_max;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -18,15 +37,16 @@ int main(int argc, char* argv[]) {
     unsigned int niter = 1000;
     double seconds_cumul = 0.;
     double seconds_to_compute_one_iteration = 0.2 / size;
+    double overhead_percent_max = 100.;
 
     cout << "iter / niter seconds_cumul" << endl;
     for (unsigned int iter = 0 ; iter < niter ; iter++)
     {
-        // Generate a number between 0 and 0.05
-        double overhead_percent = (rand() % 6) / 100.;
+        // Get overhead beetween 0. and overhead_percent_max.
+        double overhead = get_overhead_percent(rank,size,iter,
+                                               overhead_percent_max) / 100.;
         
-        double seconds = seconds_to_compute_one_iteration
-                       * (1 + overhead_percent);
+        double seconds = seconds_to_compute_one_iteration * (1 + overhead);
         unsigned int microseconds = (unsigned int) (seconds * micro);
         seconds = (double) microseconds / micro;
 
